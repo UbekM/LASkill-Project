5a06575a042624fce3227cbd9185dfdfd880d280
UbekM <ubekmichael@gmail.com>
2024-05-19 23:05:35 +0100

    javascript completed. Next is responsiveness

--- js/index.js
+++ js/index.js
@@ -2,13 +2,17 @@
 
 const wallet_button = document.getElementById("wallet-button");
 const backdrop = document.getElementById("backdrop");
+const wallet = document.getElementById("wallet");
+const svg = document.getElementById("svg");
 
 wallet_button.addEventListener("click", () => {
   backdrop.style.display = "block";
+  document.body.style.overflow = "hidden";
 });
 
 document.addEventListener("click", (event) => {
-  if (event.target !== wallet_button && event.target !== wallet) {
-    wallet.style.display = "none";
+  if (event.target == backdrop || event.target == svg) {
+    backdrop.style.display = "none";
+    document.body.style.overflow = "visible";
   }
 });

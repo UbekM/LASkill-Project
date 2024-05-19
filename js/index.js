26d92ca17e76be018eaa449e926a0bc4a69f9964
UbekM <ubekmichael@gmail.com>
2024-05-19 21:24:34 +0100

    still building

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 /** @format */
+
+const wallet_button = document.getElementById("wallet-button");
+const backdrop = document.getElementById("backdrop");
+
+wallet_button.addEventListener("click", () => {
+  backdrop.style.display = "block";
+});
+
+document.addEventListener("click", (event) => {
+  if (event.target !== wallet_button && event.target !== wallet) {
+    wallet.style.display = "none";
+  }
+});

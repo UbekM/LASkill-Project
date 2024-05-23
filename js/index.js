330bbe50156c0aa39cdfb7cd9ef58c8d7180553c
UbekM <ubekmichael@gmail.com>
2024-05-23 18:47:14 +0100

    js work

--- js/index.js
+++ js/index.js
@@ -4,6 +4,19 @@ const wallet_button = document.getElementById("wallet-button");
 const backdrop = document.getElementById("backdrop");
 const wallet = document.getElementById("wallet");
 const svg = document.getElementById("svg");
+const hamburgerIcon = document.getElementById("logo2");
+const menu = document.getElementById("menu2");
+const searchIcon = document.getElementById("search-top");
+const search = document.getElementById("search-container");
+
+function toggleMenu() {
+  if (menu.style.display === "none") {
+    menu.style.display = "block";
+    console.log(menu.style.display);
+  } else {
+    menu.style.display = "none";
+  }
+}
 
 wallet_button.addEventListener("click", () => {
   backdrop.style.display = "block";
@@ -15,4 +28,10 @@ document.addEventListener("click", (event) => {
     backdrop.style.display = "none";
     document.body.style.overflow = "visible";
   }
+});
+
+hamburgerIcon.addEventListener("click", toggleMenu);
+
+searchIcon.addEventListener("click", () => {
+  search.style.display = "block";
 });
